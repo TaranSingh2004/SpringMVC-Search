708a03fbd6c12e8c35a9b945071d55fcd8c809ac
TaranSingh2004 <taranduggal2004@gmail.com>
2025-01-15 00:56:21 +0530

    Exception handling

--- src/main/java/springmvcsearch/controller/SearchController.java
+++ src/main/java/springmvcsearch/controller/SearchController.java
@@ -1,9 +1,9 @@
 package springmvcsearch.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
 
@@ -14,12 +14,20 @@ public class SearchController {
     public String getUserDetails(@PathVariable("userId") int userId, @PathVariable("userName") String userName){
         System.out.println(userName);
         System.out.println(userId);
+
+        //for exception testing
+        Integer.parseInt(userName);
         return "home";
     }
 
     @RequestMapping("/home")
     public String home(){
         System.out.println("going to the home view");
+
+        //for testing exception handler
+        String url = null;
+        System.out.println(url.length());
+
         return "home";
     }
 
@@ -31,6 +39,24 @@ public class SearchController {
         RedirectView redirectView=new RedirectView();
         redirectView.setUrl(url);
         return redirectView;
+    }
+
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(value=NullPointerException.class)
+    public String exceptionHandlerNull(Model m){
+        m.addAttribute("msg", "Null pointer exception has occurred");
+        return "null_page";
+    }
+    @ExceptionHandler(value=NumberFormatException.class)
+    public String exceptionHandlerNumberFormat(Model m){
+        m.addAttribute("msg", "Number Format exception has occurred");
+        return "null_page";
+    }
+
+    @ExceptionHandler(value=NumberFormatException.class)
+    public String exceptionHandlerGeneric(Model m){
+        m.addAttribute("msg", "exception has occurred");
+        return "null_page";
     }
 
 }
